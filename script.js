3790e7e5d1e7696bffca70e1c2c9e553b5a5885e
Felipe <felipeprado789@gmail.com>
2022-11-28 20:30:05 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let
+function name() {
+    
+}
+
+
+'use strict';
+function typeWriter(el) {
+    const textArray = el.innerHTML.split('');
+    el.innerHTML = '';
+    textArray.forEach(letter, i => {
+        setTimeout(() => (el.innerHTML += letter), 95 * i );
+        
+    });
+}
+typeWriter(ElementEl);
+<button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me!</button>
+
+
+
+
